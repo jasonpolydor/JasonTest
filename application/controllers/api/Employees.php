a68a2eb8aba25b68726719ec7aa5439fbaa47531
Jason <jasonp@smartz-solutions.com>
2020-07-27 19:54:30 +0400

    Test HR- Jason Polydor

--- application/controllers/api/Employees.php
+++ application/controllers/api/Employees.php
@@ -5,67 +5,11 @@ use Restserver\Libraries\REST_Controller;
 
 class Employees extends REST_Controller
 {
-	public function index_get($id = NULL) {
-		$this->db->select("*");
-		$this->db->from("employees");
-		$query = $this->db->get();        
-		$employees = $query->result();
-
-        if ($id === NULL)
-        {
-            if ($employees)
-            {
-                $this->response($employees, 200);
-            }
-            else
-            {
-                $this->response([
-                    'status' => false,
-                    'message' => 'No employees were found'
-                ], 404);
-            }
-        }
-        else
-        {
-            if (array_key_exists($id, $employees))
-            {
-                $this->response($employees[$id], 200);
-            }
-            else
-            {
-                $this->response([
-                    'status' => false,
-                    'message' => 'No such employee found'
-                ], 404);
-            }
-        }
+	public function index_get($id = null) {
+		// TODO implement method
 	}
 
 	public function index_post() {
-		$employee = [
-			'first_name' => $this->post('first_name'),
-			'last_name' => $this->post('last_name'),
-			'position_name' => $this->post('position_name'),
-			'email' => $this->post('email'),
-			'telephone_number' => $this->post('telephone_number'),
-			'address' => $this->post('address'),
-		];
-
-		if($employee){
-			$result = $this->db->insert('employees',$employee);
-			$last_inserted_id = $this->db->insert_id();
-
-			if($result){
-				$this->response([
-					'message' => 'OK',
-					'rows_inserted' => $last_inserted_id
-                ], 200);
-			}else{
-				$this->response([
-                    'status' => false,
-                    'message' => 'KO'
-                ], 404);
-			}
-		}
+		// TODO implement method
 	}
 }
